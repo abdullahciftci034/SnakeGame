525b6d9a406d5c910341a6c942a3ca8b7be2f5ee
Abdullah çiftçi <abdullahciftci034@gmail.com>
2020-02-06 22:17:29 +0300

    Son Hal

--- java-code/snakeGame.java
+++ java-code/snakeGame.java
@@ -1,3 +1,4 @@
+package snakegame;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
@@ -9,18 +10,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class snakeGame extends JPanel implements KeyListener{
+    //yilanlar覺n par癟alar覺n konumlar覺n覺 tuttuk
     int Xler[],Xler1[];
     int Yler[],Yler1[];
-    int boyut=15;
-    int yilanSayisi=4;
-    int Xyon=20;
-    int Yyon=0;
-    int pencereBoyut=405;
-    int cikart;
-    int appleX;
-    int appleY;
-    int saniyede愮erlemeZamani=1;
+    
+    int pencereBoyut=405,yilanBoyut=15,Yyon=0,Xyon=20;
+    int yenilemeMiktari=3,yilanSayisi=1,appleX,appleY,cikart;
+    boolean continues=true,continues1=false;
     Random rnd=new Random();
+    
     public static void main(String[] args) {
         // TODO code application logic here
         JFrame jf=new JFrame();
@@ -30,64 +28,91 @@ public class snakeGame extends JPanel implements KeyListener{
         jf.setVisible(true);
         jf.setSize(420,450);
         jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
-        
-    
     }
     
     public snakeGame(){
-     this.Xler=new int[20];
-     this.Yler=new int[20];
-     this.Xler1=new int[20];
-     this.Yler1=new int[20];
+     this.Xler=new int[50];
+     this.Yler=new int[50];
+     this.Xler1=new int[50];
+     this.Yler1=new int[50];
      this.Xler[0]=5;
      this.Yler[0]=5;
-     this.Xler[1]=5;
-     this.Yler[1]=5;
-     this.Xler[2]=5;
-     this.Yler[2]=5;
-     this.Xler1[1]=5;
-     this.Yler1[1]=5;
-     this.Xler1[2]=5;
-     this.Yler1[2]=5;
-     this.cikart=this.pencereBoyut-5;
-     this.appleX=rnd.nextInt(20)*(this.boyut+5)+5;
-     this.appleY=rnd.nextInt(20)*(this.boyut+5)+5;
+     this.cikart=this.pencereBoyut;
+     this.appleX=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
+     this.appleY=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
      this.loop();
     }
+    
     public void loop(){
         again ag=new again(this);
         Thread t1=new Thread(ag);
         t1.start();
     }
+    
     public void update1(){
-        
-        for(int i=0;i<this.yilanSayisi-1;i++){
-            this.Xler[i+1]=this.Xler1[i];
-            this.Yler[i+1]=this.Yler1[i];
-            System.out.println(this.Yler[i+1]);
-        }
+        //hareket Yapt覺k
         this.Xler[0]+=this.Xyon;
         this.Yler[0]+=this.Yyon;
         
-        ///eski arrayi update ettik
-        for(int i=0;i<yilanSayisi;i++){
-            this.Xler1[i]=this.Xler[i];
-            this.Yler1[i]=this.Yler[i];
+        for(int i=1;i<this.yilanSayisi;i++){
+            if(this.Xler[0]==this.Xler[i] && this.Yler[0]==this.Yler[i]){
+                this.reset1();
+            }
+        }
+        
+        //elma yeme yapt覺k
+        if(this.Xler[0]==this.appleX && this.Yler[0]==this.appleY){
+            this.continues=true;
+            this.yilanSayisi++;
+            this.Xler[yilanSayisi-1]=this.Xler1[yilanSayisi-2];
+            this.Yler[yilanSayisi-1]=this.Yler1[yilanSayisi-2];
+            while(this.continues){
+                this.continues1=false;
+                this.appleX=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
+                this.appleY=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
+                for(int i=0;i<this.yilanSayisi;i++){
+                    if(this.Xler[i]==this.appleX && this.Yler[i]==this.appleY){
+                        this.continues1=true;
+                    }
+                }
+                if(!(this.continues1)){
+                    this.continues=false;
+                }
+            }
         }
         
         
+        //girintileri ve 癟覺k覺tnt覺lar覺  yapt覺k
         if(this.Xler[0]>=this.cikart){
             this.Xler[0]=5;
         }
         if(this.Yler[0]>=this.cikart){
             this.Yler[0]=5;
         }
-        if(this.Yler[0]<=0){
-           this.Yler[0]=cikart;
+        if(this.Xler[0]<=0){
+           this.Xler[0]=this.cikart;
         }
         if(this.Yler[0]<=0){
-            this.Yler[0]=cikart;
+            this.Yler[0]=this.cikart;
         }
+        //bir 繹nceki konumlar覺 ald覺k
+        for(int i=0;i<this.yilanSayisi-1;i++){
+            this.Xler[i+1]=this.Xler1[i];
+            this.Yler[i+1]=this.Yler1[i];
+            System.out.println(this.Yler[i+1]);
+        }
+        ///eski arrayi update ettik
+        for(int i=0;i<yilanSayisi;i++){
+            this.Xler1[i]=this.Xler[i];
+            this.Yler1[i]=this.Yler[i];
+        }
+    }
+    public void reset1(){
+        this.Xler[0]=5;
+        this.Yler[0]=5;
+        this.yilanSayisi=1;
+        this.appleX=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
+        this.appleY=rnd.nextInt(20)*(this.yilanBoyut+5)+5;
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -95,10 +120,10 @@ public class snakeGame extends JPanel implements KeyListener{
         g.fillRect(0,0,this.pencereBoyut,this.pencereBoyut);
         g.setColor(Color.yellow);
         for(int i=0;i<yilanSayisi;i++){
-            g.fillRect(this.Xler[i],this.Yler[i],this.boyut,this.boyut); 
+            g.fillRect(this.Xler[i],this.Yler[i],this.yilanBoyut,this.yilanBoyut); 
         }
         g.setColor(Color.PINK);
-        g.fillRect(this.appleX,this.appleY,this.boyut, this.boyut);
+        g.fillRect(this.appleX,this.appleY,this.yilanBoyut, this.yilanBoyut);
     }
 
     @Override
@@ -135,7 +160,7 @@ public class snakeGame extends JPanel implements KeyListener{
         public void run(){
             while(true){
                 try {
-                    Thread.sleep(1000/sg.saniyede愮erlemeZamani);
+                    Thread.sleep(1000/sg.yenilemeMiktari);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(snakeGame.class.getName()).log(Level.SEVERE, null, ex);
                 }
